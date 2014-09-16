063f2a5b4854717a203046ea6781d045facd7934
Jesse Jackson <jesse.publicmail@gmail.com>
2014-09-16 15:14:53 -0500

    v1.1.3 Executes only if current with site content
    If outdated, will notify in console

--- extension/js/user.js
+++ extension/js/user.js
@@ -1,12 +1,19 @@
-var p = document.getElementById('password');
-var u = document.getElementById('userid');
-
-u.addEventListener('change', storeID);
-if (!(localStorage.getItem('userid') === null) && !(localStorage.userid == '')) { // Exists and not empty
-	fillID();
-	} else { // Does not exist or is empty
-		u.focus();
+if (document.getElementById('userid') && document.getElementById('password')) {
+	var p = document.getElementById('password');
+	var u = document.getElementById('userid');
+	init();
+	} else {
+		console.log('Chrome extension "Wells Fargo - Remember username" is out of date and is no longer functioning. Developer must update.');
 		}
+
+function init() {
+	u.addEventListener('change', storeID);
+	if (!(localStorage.getItem('userid') === null) && !(localStorage.userid == '')) { // localStorage key exists and is not empty
+		fillID();
+		} else { // Does not exist or is empty
+			u.focus();
+			}
+	}
 
 function fillID() {
 	u.value = localStorage.userid;
