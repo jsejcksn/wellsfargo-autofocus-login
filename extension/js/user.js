441dcf6f3aba9ebefed827c51f192bd9b668fc91
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-09-16 03:09:10 -0500

    Make IIFE

--- extension/js/user.js
+++ extension/js/user.js
@@ -1,25 +1,29 @@
-if (document.getElementById('userid') && document.getElementById('password')) { // Username and password inputs exist
-  var p = document.getElementById('password');
-  var u = document.getElementById('userid');
-  init();
-} else {
-  console.log('Chrome extension "Wells Fargo - Remember username" is out of date and is no longer functioning. Developer must update.'); // Wells Fargo homepage has been updated, breaking extension functionality
-}
+(function() {
 
-function init() {
-  u.addEventListener('change', storeID);
-  if (!(localStorage.getItem('userid') === null || undefined || '' || 0)) { // localStorage key exists and is not empty
-    fillID();
-  } else { // Does not exist or is empty
-    u.focus();
+  if (document.getElementById('userid') && document.getElementById('password')) { // Username and password inputs exist
+    var p = document.getElementById('password');
+    var u = document.getElementById('userid');
+    init();
+  } else {
+    console.log('Chrome extension "Wells Fargo - Remember username" is out of date and is no longer functioning. Developer must update.'); // Wells Fargo homepage has been updated, breaking extension functionality
   }
-}
 
-function fillID() {
-  u.value = localStorage.userid;
-  p.focus();
-}
+  function init() {
+    u.addEventListener('change', storeID);
+    if (!(localStorage.getItem('userid') === null || undefined || '' || 0)) { // localStorage key exists and is not empty
+      fillID();
+    } else { // Does not exist or is empty
+      u.focus();
+    }
+  }
 
-function storeID() {
-  localStorage.setItem('userid', u.value);
-}
+  function fillID() {
+    u.value = localStorage.userid;
+    p.focus();
+  }
+
+  function storeID() {
+    localStorage.setItem('userid', u.value);
+  }
+
+}());
