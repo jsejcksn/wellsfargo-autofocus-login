862846d665c2b042ec1229b5e297cd9e793b8d6b
Jesse Jackson <jsejcksn@users.noreply.github.com>
2015-09-16 04:00:53 -0500

    Enhance username conditional, Cleanup code

--- extension/js/user.js
+++ extension/js/user.js
@@ -1,19 +1,12 @@
 (function() {
-	'use strict';
-
-  if (document.getElementById('userid') && document.getElementById('password')) { // Username and password inputs exist
-    var p = document.getElementById('password');
-    var u = document.getElementById('userid');
-    init();
-  } else {
-    console.log('Chrome extension "Wells Fargo - Remember username" is out of date and is no longer functioning. Developer must update.'); // Wells Fargo homepage has been updated, breaking extension functionality
-  }
+  'use strict';
 
   function init() {
     u.addEventListener('change', storeID);
-    if (!(localStorage.getItem('userid') === null || undefined || '' || 0)) { // localStorage key exists and is not empty
+    var userid = localStorage.getItem('userid');
+    if (userid !== null && typeof userid !== 'undefined' && userid !== '' && userid !== ' ') { // localStorage key exists and is not empty or a single space
       fillID();
-    } else { // Does not exist or is empty
+    } else { // Does not exist or is empty or a single space
       u.focus();
     }
   }
@@ -25,6 +18,14 @@
 
   function storeID() {
     localStorage.setItem('userid', u.value);
+  }
+
+  if (document.getElementById('userid') && document.getElementById('password')) { // Username and password inputs exist
+    var p = document.getElementById('password');
+    var u = document.getElementById('userid');
+    init();
+  } else {
+    console.log('Chrome extension "Wells Fargo - Remember username" is out of date and is no longer functioning. Developer must update.'); // Wells Fargo homepage has been updated, breaking extension functionality
   }
 
 }());
